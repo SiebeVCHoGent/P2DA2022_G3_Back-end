9aac0e7e2f1482ffeb4f9b5ef6d7f2861b529b54
Jarno Beeckman <79344588+JarnoBeeckmanhogent@users.noreply.github.com>
2022-05-05 11:48:45 +0200

    whitespace fix

--- src/repository/kmo.js
+++ src/repository/kmo.js
@@ -2,7 +2,7 @@ const { tables, getKnex } = require('../data');
 const { getChildLogger } = require('../core/logging');
 
 const search = async (str)=>{
-    return await getKnex().raw("SELECT kmo.*,gemeente.naam as gemeente, sector.naam as sector FROM kmo join sector on kmo.sectorid = sector.id join gemeente on gemeente.postcode = kmo.postcode WHERE ondernemingsnummer LIKE ? or kmo.naam LIKE ? limit 25;",[`%${str}`, `%${str}%`])
+    return await getKnex().raw("SELECT kmo.*,gemeente.naam as gemeente, sector.naam as sector FROM kmo join sector on kmo.sectorid = sector.id join gemeente on gemeente.postcode = kmo.postcode WHERE ondernemingsnummer LIKE ? or kmo.naam LIKE ? limit 25;",[`%${str.replace(/\s/g, "")}`, `%${str}%`])
 }
 
 module.exports = {
